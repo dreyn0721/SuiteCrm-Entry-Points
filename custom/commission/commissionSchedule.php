050234469451144958d9ee63df8411a403a6f9cd
dreyn0721 <47095704+dreyn0721@users.noreply.github.com>
2023-07-25 05:23:20 +0800

    Update commissionSchedule.php

--- custom/commission/commissionSchedule.php
+++ custom/commission/commissionSchedule.php
@@ -34,7 +34,7 @@ $commission = new Med01_Commission();
 
 //////////////////INSURED/////////////
 // Get all the list, no limit, we only limit the number of to updates
-$query = "SELECT id, name, hra, commissionschedule, chargeback, first, renewal FROM {$commission->table_name} WHERE deleted != '1' ";
+$query = "SELECT id, name, commissionschedule, chargeback, first, renewal, deleted FROM {$commission->table_name} WHERE deleted != '1' ";
 $result = $commission->db->query($query);
 
 
@@ -43,12 +43,13 @@ $total_rows_updated = 0;
 
 $has_update = false; //zxc
 $update_data = []; //zxc
+$remaining = 0;
 
 while ($row = $commission->db->fetchByAssoc($result)) {
 
     $formula = '';
     if( isset( $row['chargeback'] ) && $row['chargeback'] !== 0 && !is_null( $row['chargeback'] ) ){
-        $formula = 'chargeback';
+        $formula = 'Chargeback';
     } else {
         if( isset( $row['first'] ) && ( $row['first'] == "true" || $row['first'] == true || $row['first'] == "1" || $row['first'] == 1 ) ){
             $formula = 'first';
@@ -61,14 +62,26 @@ while ($row = $commission->db->fetchByAssoc($result)) {
         }
     }
 
+    if( is_null( $row['commissionschedule'] ) ){
+        $row['commissionschedule'] = "";
+    }
 
-    if( isset( $formula ) && ( isset( $row['commissionschedule'] ) || is_null( $row['commissionschedule'] ) ) ){ //zxc
+    if( isset( $formula ) && isset( $row['commissionschedule'] ) ){ //zxc
         if( strtolower( str_replace(" ", "", (string)$row['commissionschedule']) ) != strtolower( str_replace(" ", "", (string)$formula ) ) ){
-        	echo strtolower( str_replace(" ", "", (string)$row['commissionschedule']) ) ." ==? ". strtolower( str_replace(" ", "", (string)$formula ) );
-            exit();
+            // if( $row['id'] == '100001'){
+
+            //     echo "ID:".$row['id']."\n<br>";
+            //     echo strtolower( str_replace(" ", "", (string)$row['commissionschedule']) ) ." ==? ". strtolower( str_replace(" ", "", (string)$formula ) );
+            //     exit();
+            // }
             //zxc
+            if( count( $update_data ) >= 10000 ){
+                $remaining++;
+                continue;
+            }
+
             $has_update = true;
-            $update_data[] = '("'.$row['id'].'", '.$formula.')'; 
+            $update_data[] = '("'.$row['id'].'", "'.$formula.'")'; 
             $total_rows_updated++;// Count the total row updated
 
         }
@@ -84,9 +97,9 @@ if( isset( $has_update ) && $has_update && isset( $update_data ) && is_array( $u
 
     $result = $commission->db->query($update_query);
 
-
     echo $total_rows_updated." total rows has been updated<br>";
-    echo 'Recently changed or created records updated successfully.';
+    echo 'Recently changed or created records updated successfully.<br>';
+    echo $remaining." rows remaining that needed to update, will be executed in next load.";
 
 
 } else {
